69e4b4f92429189cad5a9ae624e1f73c9e9eabb7
Zagrebin Egor <zagrebine@yandex.ru>
2026-05-16 23:14:35 +0300

    utils

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ def load_model(model_path):
         raise FileNotFoundError(f"Model not found: {model_path}")
     return joblib.load(model_path)
 
-def save_model(model, save_path):
+def save_model(model, model_path):
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
     joblib.dump(model, model_path)
     print(f"✅ Model saved to {model_path}")
