b1b3c48ec7c0d62b2ec1e8617ab4f6f143c73b93
Zagrebin Egor <zagrebine@yandex.ru>
2026-05-21 01:46:57 +0300

    preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,7 +4,7 @@ from sklearn.compose import ColumnTransformer
 import numpy as np
 
 def get_preprocessor(config):
-    """Creating data preprocessor"""
+    """EDA preprocesso for ipynb"""
     scale_cols = config['preprocess'].get('scale_cols', [])
     log_cols = config['preprocess'].get('log_cols', [])
     
@@ -22,3 +22,23 @@ def get_preprocessor(config):
     
     preprocessor = ColumnTransformer(transformers, remainder='drop')
     return preprocessor
+
+def get_new_preprocessor(config):
+    """New pipeline for train.py"""
+
+    scale_cols = config['preprocess']['scale_cols']
+    log_cols = config['preprocess']['log_cols']
+
+    scale_pipeline = Pipeline([
+        ('Scaler', StandardScaler()), 
+    ])
+    log_pipeline = Pipeline([
+        ('Log', FunctionTransformer(np.log1p, validate = True)), 
+        ('Scaler', StandardScaler())
+    ])
+
+    pipeline = ColumnTransformer([
+        ('Log', log_pipeline, log_cols), 
+        ('Scale', scale_pipeline, scale_cols)
+    ])
+    return pipeline
